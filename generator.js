35b72add9c797b569cf24327ba1ef3311fec2a7d
Chris Strom <github@eeecomputes.com>
2014-09-02 00:04:34 -0400

    Run the generator post-install

--- generator.js
+++ generator.js
@@ -1,18 +1,42 @@
 #!/usr/bin/env node
 
+if (/node_modules\/eee-polymer-tests/.test(process.cwd())) {
+  // Need to run the generator from the element package, not from the
+  // installed node module
+  process.chdir('../..');
+}
+
 var fs = require('fs');
+var readline = require('readline');
 
-var element = process.argv[2];
-var camel = element.
-  split(/-/).
-  map(function(t) { return t.charAt(0).toUpperCase() + t.slice(1); }).
-  join('');
+var element, camel;
 
-// checkForExistingFiles();
-ensureTestDirectory();
-generateKarmaConf();
-generatePolymerSetup();
-generateTestSkeleton();
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question("What is the name of the Polymer element being tested? ", function(answer) {
+  element = answer.replace(/[<>]*/g, '');
+  camel = element.
+    split(/-/).
+    map(function(t) { return t.charAt(0).toUpperCase() + t.slice(1); }).
+    join('');
+
+  console.log("Generating test setup for: ", element);
+  generate();
+  console.log("Done!");
+
+  rl.close();
+});
+
+function generate() {
+  // checkForExistingFiles();
+  ensureTestDirectory();
+  generateKarmaConf();
+  generatePolymerSetup();
+  generateTestSkeleton();
+}
 
 function checkForExistingFiles() {
   console.log("Not implemented");

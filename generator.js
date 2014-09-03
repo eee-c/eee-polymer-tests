db8818a4c32229b90732276f4d48c3ba6f631805
Chris Strom <github@eeecomputes.com>
2014-09-03 00:28:13 -0400

    Don't overwrite files, unless explicitly told to do so

--- generator.js
+++ generator.js
@@ -8,39 +8,31 @@ if (/node_modules\/eee-polymer-tests/.test(process.cwd())) {
 
 var fs = require('fs');
 var readline = require('readline');
+var parseArgs = require('minimist');
+require('colors');
 
-var element, camel;
+var KARMA_CONF = 'karma.conf.js';
+var POLYMER_SETUP = 'test/PolymerSetup.js';
 
-var rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+var element, force;
 
-rl.question("What is the name of the Polymer element being tested? ", function(answer) {
-  element = answer.replace(/[<>]*/g, '');
-  camel = element.
-    split(/-/).
-    map(function(t) { return t.charAt(0).toUpperCase() + t.slice(1); }).
-    join('');
-
-  console.log("Generating test setup for: ", element);
+parseCommandLine();
+if (element) {
   generate();
-  console.log("Done!");
-
-  rl.close();
-});
-
-function generate() {
-  // checkForExistingFiles();
-  ensureTestDirectory();
-  generateKarmaConf();
-  generatePolymerSetup();
-  generateTestSkeleton();
+}
+else {
+  queryForElementName();
 }
 
-function checkForExistingFiles() {
-  console.log("Not implemented");
-  process.exit(1);
+function generate() {
+  console.log("\nGenerating test setup for: " + element);
+
+  ensureTestDirectory();
+  if (okKarmaConf()) generateKarmaConf();
+  if (okPolymerSetup()) generatePolymerSetup();
+  if (okTestSkeleton()) generateTestSkeleton();
+
+  console.log("Done!\n");
 }
 
 function ensureTestDirectory() {
@@ -50,7 +42,11 @@ function ensureTestDirectory() {
 
 function generateKarmaConf() {
   fs.createReadStream(__dirname + '/templates/karma.conf.js').
-    pipe(fs.createWriteStream('karma.conf.js'));
+    pipe(fs.createWriteStream(KARMA_CONF));
+}
+
+function okKarmaConf() {
+  return _okOverwrite(KARMA_CONF);
 }
 
 function generatePolymerSetup() {
@@ -61,10 +57,13 @@ function generatePolymerSetup() {
       process.exit(1);
     }
 
-    var filename = './test/PolymerSetup.js';
     var content = data.replace(/\{\{element-name\}\}/g, element);
-    fs.writeFile(filename, content);
+    fs.writeFile(POLYMER_SETUP, content);
   });
+}
+
+function okPolymerSetup() {
+  return _okOverwrite(POLYMER_SETUP);
 }
 
 function generateTestSkeleton() {
@@ -75,8 +74,79 @@ function generateTestSkeleton() {
       process.exit(1);
     }
 
-    var filename = './test/' + camel + 'Spec.js';
     var content = data.replace(/\{\{element-name\}\}/g, element);
-    fs.writeFile(filename, content);
+    fs.writeFile(_testFilename(), content);
   });
+}
+
+function okTestSkeleton() {
+  return _okOverwrite(_testFilename());
+}
+
+function _testFilename() {
+  var camel = element.
+    split(/-/).
+    map(function(t) { return t.charAt(0).toUpperCase() + t.slice(1); }).
+    join('');
+
+  return  'test/' + camel + 'Spec.js';
+}
+
+function _okOverwrite(filename) {
+  if (!fs.existsSync(filename)) return true;
+
+  if (force) {
+    var message = 'Force overwrite: ' + filename + '.';
+    console.log(message);
+    return true;
+  }
+
+  var message = '[WARN] File exists: ' + filename + '.' +
+                ' (use --force to overwrite)';
+  console.log(message.yellow);
+  return false;
+}
+
+function parseCommandLine() {
+  if (process.argv.length <= 2) return;
+  var argv = parseArgs(process.argv.slice(2), {
+    'boolean': 'force help'
+  });
+  if (argv._.length != 1) return;
+
+  if (argv.help) {
+    showHelp();
+    process.exit(0);
+  }
+
+  element = argv._[0];
+  force = argv.force;
+}
+
+function queryForElementName() {
+  var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+  rl.question("What is the name of the Polymer element being tested? ",
+   function(answer) {
+     element = answer.replace(/[<>]*/g, '');
+
+     generate();
+     showHelp();
+
+     rl.close();
+  });
+}
+
+function showHelp() {
+  console.log(
+    (
+      "The Polymer test generator can be re-run with: " +
+      "./node_modules/eee-polymer-tests/generator.js <x-element-name>\n"
+    ).
+      green.
+      bold
+  );
 }
